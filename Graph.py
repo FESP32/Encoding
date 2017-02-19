08d8db745bc6e20cf3795f9d15db396be599b616
Fernando Sanchez <fesp321@gmail.com>
2017-02-18 23:48:27 -0600

    deleted some comments

--- Graph.py
+++ Graph.py
@@ -52,27 +52,7 @@ plt.legend()
 plt.title('Manchester, HDB3 & B3ZS Encoding')
 plt.show()
 
-# x = np.arange(1, 7, 0.4)
-# y0 = np.sin(x)
-# y = y0.copy() + 2.5
-#
-# plt.step(x, y, label='pre (default)')
-#
-# y -= 0.5
-# plt.step(x, y, where='mid', label='mid')
-#
-# y -= 0.5
-# plt.step(x, y, where='post', label='post')
-#
-# y = ma.masked_where((y0 > -0.15) & (y0 < 0.15), y - 0.5)
-# plt.step(x, y, label='masked (pre)')
-#
-# plt.legend()
-#
-# plt.xlim(0, 7)
-# plt.ylim(-0.5, 4)
-#
-# plt.show()
+
 
 
 
